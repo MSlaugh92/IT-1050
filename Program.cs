a9cd7fbaae03dc0986075bbd456cd83815b43f57
MSlaugh92 <59938039+MSlaugh92@users.noreply.github.com>
2020-04-28 14:34:07 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
-﻿
-using System;
+﻿using System;
 
 
 
-namespace Assignment3
+namespace Assignment4
 
 {
 
@@ -15,47 +14,199 @@ namespace Assignment3
 
         {
 
-            Instructor John = new Instructor("John", "English");
+            MovieTime movieTime = new MovieTime();
 
-            Instructor Mike = new Instructor("Mike", "Math");
-
-
-
-            Student Jane = new Student("Jane", John);
-
-            Student Joe = new Student("Joe", John);
+            string input;
 
 
 
-            Student Melissa = new Student("Melissa", Mike);
-
-            Student Matt = new Student("Matt", Mike);
+            DisplayHeader();
 
 
 
-            John.SetStudentGrade(Jane, 95);
+            Console.Write("Are you bringing anybody? (yes/no): ");
 
-            John.SetStudentGrade(Joe, 85);
+            input = Console.ReadLine();
 
+            if (input.ToLower() == "yes")
 
+            {
 
-            Mike.SetStudentGrade(Melissa, 90);
-
-            Mike.SetStudentGrade(Matt, 92);
-
-
-
-            Jane.Print();
-
-            Joe.Print();
-
-            Melissa.Print();
-
-            Matt.Print();
+                DisplayHeader();
 
 
 
-            Console.ReadKey();
+                Console.Write("How many people (including yourself) are coming?: ");
+
+                movieTime.people = int.Parse(Console.ReadLine());
+
+
+
+                Console.Write("How many are children?: ");
+
+                movieTime.children = int.Parse(Console.ReadLine());
+
+
+
+                Console.Write("How many are seniors?: ");
+
+                movieTime.seniors = int.Parse(Console.ReadLine());
+
+
+
+                movieTime.adults = movieTime.people - movieTime.children - movieTime.seniors;
+
+            }
+
+            else if (input.ToLower() == "no")
+
+            {
+
+                DisplayHeader();
+
+
+
+                Console.WriteLine("Ticket for one? Enjoy!");
+
+                Console.Write("Press any key to continue...");
+
+                Console.ReadLine();
+
+            }
+
+            else
+
+            {
+
+                DisplayHeader();
+
+
+
+                Console.WriteLine(" Invalid Entry! ");
+
+                Console.Write("Press any key to continue...");
+
+            }
+
+
+
+            DisplayHeader();
+
+            Console.Write("Do you want to see a matinee or an evening film? (matinee/evening): ");
+
+            input = Console.ReadLine();
+
+            if (input.ToLower() == "matinee")
+
+            {
+
+                movieTime.matinee = true;
+
+            }
+
+            else if (input.ToLower() == "evening")
+
+            {
+
+                movieTime.matinee = false;
+
+            }
+
+            else
+
+            {
+
+                DisplayHeader();
+
+
+
+                Console.WriteLine("Invalid I will assume matinee since it is cheaper");
+
+                Console.Write("Press any key to continue...");
+
+                Console.ReadLine();
+
+                movieTime.matinee = true;
+
+            }
+
+
+
+            DisplayHeader();
+
+            Console.WriteLine("Gotta have snacks! Enter quantities in whole numbers.\n");
+
+            Console.Write("How many small sodas?: ");
+
+            movieTime.smSodas = int.Parse(Console.ReadLine());
+
+            Console.Write("How many large sodas?: ");
+
+            movieTime.lgSodas = int.Parse(Console.ReadLine());
+
+            Console.Write("How many hot dogs?: ");
+
+            movieTime.hotDogs = int.Parse(Console.ReadLine());
+
+            Console.Write("How many popcorns?: ");
+
+            movieTime.popcorns = int.Parse(Console.ReadLine());
+
+            Console.Write("How many candies?: ");
+
+            movieTime.candies = int.Parse(Console.ReadLine());
+
+
+
+            DisplayHeader();
+
+            Console.WriteLine("Your total before coupons would be: {0:C}\n", movieTime.CalculateTotal());
+
+
+
+            movieTime.Promotions();
+
+
+
+            Console.WriteLine("After coupons, your total will be: {0:C}", movieTime.totalPrice);
+
+
+
+            if (movieTime.freePopcorn)
+
+            {
+
+                Console.WriteLine("You may also get a free popcorn!");
+
+            }
+
+
+
+            if (movieTime.freeCandies > 0)
+
+            {
+
+                Console.WriteLine("You will also get {0} free candies!", movieTime.freeCandies);
+
+            }
+
+
+
+            Console.Write("Press any key to exit...");
+
+            Console.ReadLine();
+
+        }
+
+
+
+        static void DisplayHeader()
+
+        {
+
+            Console.Clear();
+            Console.WriteLine("******NIGHT AT THE MOVIES CALCULATOR******\n");
+          
 
         }
 
